37b77dd1f7bba7d8eb3100b9feb186818f9470ea
26TQT01-TiffanyQuex <tiffanyquexdb26@gmail.com>
2026-04-28 22:29:55 -0600

    Add files via upload

---
+++ C#/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num, i;
+        Console.WriteLine("Digite un entero");
+        num = Convert.ToInt32(Console.ReadLine());
+        i = 1;
+        while (i <= 10)
+        {
+            Console.WriteLine(i+" = "+num * i);
+            i++;
+        }
+    }
+}
