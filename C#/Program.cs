d9de2522ca6b015958900ff35b727434e78e5cdd
26TQT01-TiffanyQuex <tiffanyquexdb26@gmail.com>
2026-04-28 18:53:54 -0600

    Add files via upload

---
+++ C#/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int ini= 20, x;
+        Console.WriteLine(" Ingrese un valor mayor que 20");
+        x = int.Parse(Console.ReadLine());
+
+        while (ini <= x) {
+            if (ini % 2 != 0)
+            {
+                Console.WriteLine("Los números impares son: "+ini);
+            }
+        }   ini++;
+    }
+}
