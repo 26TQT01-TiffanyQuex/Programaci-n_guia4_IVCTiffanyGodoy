b818ccb4e3bfe1a3f3c4e7d6e10a9130bf8ffc75
26TQT01-TiffanyQuex <tiffanyquexdb26@gmail.com>
2026-05-11 23:41:54 -0600

    Add files via upload

---
+++ C#/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num, result;
+        Console.WriteLine("Ingrese un valor");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= 20; i++)
+        {
+            result = i * num;
+            Console.WriteLine(result);
+        }
+    }
+}
