c53f120a76e7c22b243ec139fe7e3bfeb7e17180
26TQT01-TiffanyQuex <tiffanyquexdb26@gmail.com>
2026-05-12 00:55:14 -0600

    Add files via upload

---
+++ C#/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int entrada, i= 0;
+        Console.WriteLine("Los números enteros que ingrese se sumarán hasta que ingrese un 9 y este no se sumará");
+        entrada = Convert.ToInt32(Console.ReadLine());
+
+        if (entrada != 9)
+        {
+            do
+            {
+                entrada = i + entrada;
+                Console.ReadLine(); 
+            }
+            while (entrada != 9);
+        }
+    }
+}
