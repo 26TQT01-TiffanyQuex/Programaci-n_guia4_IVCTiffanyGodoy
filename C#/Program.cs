7ec6db34896a1cf57b175d13cdbea468fc908d7a
26TQT01-TiffanyQuex <tiffanyquexdb26@gmail.com>
2026-05-11 22:52:16 -0600

    Add files via upload

---
+++ C#/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int total=0, num, contaDivisores, divisor, primostotal=0;
+
+        for (num = 2; num <= 100; num++)
+        {
+            contaDivisores = 0;
+            for (divisor = 1; divisor <= 100; divisor++)
+            {
+                if (num%divisor == 0)
+                {
+                    contaDivisores = contaDivisores + 1;
+                }
+            }
+
+            if (contaDivisores== 2)
+            {
+                total = total + num;
+                primostotal = primostotal + 1;
+            }
+
+        }
+        Console.WriteLine("Cantidad de números primos: "+ primostotal);
+        Console.WriteLine("La suma de los números primos del 1-100 es: " + total);
+    }
+}
